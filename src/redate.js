e7aa15584561be0c1bbd46b1a128640af1d51abf
Niilo Poutanen <niilo.poutanen@gmail.com>
2026-02-28 17:29:05 +0200

    bump

--- src/redate.js
+++ src/redate.js
@@ -11,7 +11,7 @@ const program = new Command();
 program
     .name("redate")
     .description("Rename images based on EXIF dates")
-    .version("0.2.3");
+    .version("0.3.0");
 
 program
     .command("process <paths...>")
