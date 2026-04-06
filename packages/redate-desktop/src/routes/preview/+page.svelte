0a93ce2e73de8918542067b8e936044ba9f807ef
Niilo Poutanen <niilo.poutanen@gmail.com>
2026-04-06 22:00:48 +0300

    Enhancing previews

--- packages/redate-desktop/src/routes/preview/+page.svelte
+++ packages/redate-desktop/src/routes/preview/+page.svelte
@@ -22,23 +22,32 @@
 </script>
 
 <div class="preview">
-    <div class="handle">
-        {#if !isMac}
-            <div class="controls-windows">
-                <button onclick={() => window.electron.minimize("preview")}> <img src={dash} alt="Minimize" /> </button>
-                <button onclick={() => window.electron.close("preview")}> <img src={close} alt="Close" /> </button>
-            </div>
+    <div class="handle"></div>
+    <div class="files">
+        {#if files && files.length > 0}
+            {#each files as file}
+                <div class="file">
+                    <div class="icon">
+                        <img src="thum:///{file}" alt="" />
+                        <div class="remove" title="Remove from list">
+                            <img src={close} alt="Remove from list" />
+                        </div>
+                    </div>
+                    <p class="name">{getFileNameFromPath(file)}</p>
+                </div>
+            {/each}
+        {:else}
+            <p>No files selected for processing.</p>
         {/if}
     </div>
-    <div class="files">
-        {#each files as file}
-            <div class="file">
-                <div class="icon">
-                    <img src="thum:///{file}" alt="" />
-                </div>
-                <p class="name">{getFileNameFromPath(file)}</p>
-            </div>
-        {/each}
+
+    <div class="controls">
+        <button class="primary" onclick={() => window.electron.close("preview")} title="Close">
+            <p>Close</p>
+        </button>
+        <button onclick={() => window.electron.setFiles([])} title="Clear all">
+            <p>Clear all</p>
+        </button>
     </div>
 </div>
 
@@ -54,11 +63,16 @@
         overflow: hidden;
         border: 2px solid $layer-1;
         box-sizing: border-box;
+        padding: 0 5px 5px 5px;
     }
     .handle {
         width: 100%;
         height: 40px;
         -webkit-app-region: drag;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 10;
 
         .controls-windows {
             height: 100%;
@@ -78,6 +92,7 @@
                 display: flex;
                 align-items: center;
                 justify-content: center;
+                cursor: pointer;
 
                 &:hover {
                     background-color: $layer-1;
@@ -95,24 +110,118 @@
         display: flex;
         flex-wrap: wrap;
         gap: 10px;
-        padding: 0 20px 20px 20px;
+        padding: 10px 5px 80px 5px;
+        overflow-y: scroll;
 
         .file {
             display: flex;
             flex-direction: column;
             align-items: center;
             justify-content: center;
-            img {
-                background-color: $layer-2;
-                width: 100px;
+            width: 20%;
+            flex-grow: 1;
+            max-width: 24%;
+            position: relative;
+            gap: 5px;
+
+            .icon {
+                width: 100%;
+                min-width: 60px;
                 height: 80px;
-                border-radius: 4px;
-                object-fit: cover;
+                position: relative;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                img {
+                    width: auto;
+                    max-width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    border-radius: 4px;
+                }
+
+                .remove {
+                    position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    transform: translate(-50%, -50%);
+                    width: 30px;
+                    height: 30px;
+                    background-color: #00000046;
+                    backdrop-filter: blur(10px);
+                    padding: 8px;
+                    box-sizing: border-box;
+                    border-radius: 50%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    overflow: hidden;
+                    opacity: 0;
+                    pointer-events: none;
+                    transition: opacity 0.2s ease;
+                    cursor: pointer;
+
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        filter: brightness(2);
+                    }
+                }
+
+                &:hover {
+                    .remove {
+                        opacity: 1;
+                        pointer-events: all;
+                    }
+                }
             }
 
             .name {
                 margin: 0;
                 font-size: 12px;
+                text-align: center;
+            }
+        }
+
+        &::-webkit-scrollbar {
+            width: 10px;
+        }
+
+        &::-webkit-scrollbar-track {
+            background: transparent;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background: $layer-2;
+            border-radius: 10px;
+        }
+    }
+
+    .controls {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        background: linear-gradient(0deg, $layer-0 20%, #21212100 100%);
+        width: 100%;
+        padding: 10px;
+        box-sizing: border-box;
+        border-radius: 0 0 10px 10px;
+
+        button {
+            padding: 10px;
+            border-radius: 10px;
+            border: none;
+            outline: none;
+            background-color: $layer-1;
+            cursor: pointer;
+
+            &.primary {
+                background-color: $accent;
+            }
+
+            p {
+                margin: 0;
             }
         }
     }
