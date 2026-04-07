73d15d68fb4967f88de72d60e1cf99c94d5162a0
Niilo Poutanen <niilo.poutanen@gmail.com>
2026-04-07 18:33:10 +0300

    Mac path fix

--- packages/redate-desktop/vite.config.ts
+++ packages/redate-desktop/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		headers: {
+			'cache-control': 'no-store'
+		}
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
